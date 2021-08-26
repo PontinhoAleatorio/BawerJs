98167c2b7ab4281c45e17e65c71b794787fc249d
Hey Pontinho's | Criador da Bawer <84294591+PontinhoAleatorio@users.noreply.github.com>
2021-08-26 14:04:13 -0300

    Delete lixo.js

--- lixo.js
+++
@@ -1,4 +0,0 @@
- let user = message.mentions.users.first() || message.author;
-        let avatar = user.displayAvatarURL({size: 1024, dynamic: false, format: 'png' });
-        let img = await new DIG.Trash().getImage(avatar)
-        let attach = new Discord.MessageAttachment(img, 'trash.png');;
